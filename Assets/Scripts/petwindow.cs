5d28a1c704aaf126a7afaaef5e296b7953c68cd4
SuperFireBomber <122469311+SuperFireBomber@users.noreply.github.com>
2024-08-11 20:25:26 -0700

    1

--- Assets/Scripts/petwindow.cs
+++ Assets/Scripts/petwindow.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.InteropServices;
 using System;
- 
+
 public class WindowTransparent : MonoBehaviour
 {
-
     [DllImport("user32.dll")]
     public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
- 
 
     private struct MARGINS
     {
@@ -18,57 +16,45 @@ public class WindowTransparent : MonoBehaviour
         public int cyTopHeight;
         public int cyBottomHeight;
     }
- 
 
     [DllImport("user32.dll")]
     private static extern IntPtr GetActiveWindow();
- 
 
     [DllImport("Dwmapi.dll")]
     private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
- 
 
     [DllImport("user32.dll")]
     private static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
- 
 
     [DllImport("user32.dll", SetLastError = true)]
     static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
- 
 
     [DllImport("user32.dll")]
     static extern int SetLayeredWindowAttributes(IntPtr hWnd, uint crKey, byte bAlpha, uint dwFlags);
- 
-    // 代码中使用的常量和变量
+
+    // Constants and variables used in the code
     const int GWL_EXSTYLE = -20;  
     const uint WS_EX_LAYERED = 0x00080000;  
     const uint WS_EX_TRANSPARENT = 0x00000020;  
     static readonly IntPtr HWND_TOPMOST = new IntPtr(-1); 
     const uint LWA_COLORKEY = 0x00000001;  
     private IntPtr hWnd;  
- 
+
     private void Start()
     {
-
- 
 #if !UNITY_EDITOR
         hWnd = GetActiveWindow();
- 
+
         MARGINS margins = new MARGINS { cxLeftWidth = -1 };
- 
 
         DwmExtendFrameIntoClientArea(hWnd, ref margins);
- 
 
         SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED);
 
-
-        SetLayeredWindowAttributes(hWnd, 0, 0, LWA_COLORKEY);
- 
+        SetLayeredWindowAttributes(hWnd, 0x00FF00, 0, LWA_COLORKEY);
 
         SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, 0);
 #endif
- 
 
         Application.runInBackground = true;
     }
